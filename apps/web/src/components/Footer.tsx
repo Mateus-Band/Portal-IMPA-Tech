afa187cabc888324ea9c5f929b92235218125af9
Mateus Bandeira <203907751+Mateus-Band@users.noreply.github.com>
2025-11-26 16:42:26 -0300

    Delete apps/web directory

--- apps/web/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white py-4 text-center mt-10">
-      © 2025 IMPA Tech — Portal do Estudante
-    </footer>
-  );
-}
